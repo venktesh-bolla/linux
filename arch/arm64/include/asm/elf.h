683adfac9fc15101ba6c4191b0dfb65e74e46817
Yury Norov <ynorov@caviumnetworks.com>
2016-01-14 20:23:07 +0300

    arm64: introduce binfmt_elf32.c
    As we support more than one compat formats, it looks more reasonable
    to not use fs/compat_binfmt.c. Custom binfmt_elf32.c allows to move aarch32
    specific definitions there and make code more maintainable and readable.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- arch/arm64/include/asm/elf.h
+++ arch/arm64/include/asm/elf.h
@@ -166,36 +166,12 @@ extern int arch_setup_additional_pages(struct linux_binprm *bprm,
 
 #ifdef CONFIG_COMPAT
 
-#ifdef __AARCH64EB__
-#define COMPAT_ELF_PLATFORM		("v8b")
-#else
-#define COMPAT_ELF_PLATFORM		("v8l")
-#endif
-
 #define COMPAT_ELF_ET_DYN_BASE		(2 * TASK_SIZE_32 / 3)
 
 /* AArch32 registers. */
 #define COMPAT_ELF_NGREG		18
 typedef unsigned int			compat_elf_greg_t;
 typedef compat_elf_greg_t		compat_elf_gregset_t[COMPAT_ELF_NGREG];
-
-/* AArch32 EABI. */
-#define EF_ARM_EABI_MASK		0xff000000
-#define compat_elf_check_arch(x)	(((x)->e_machine == EM_ARM) && \
-					 ((x)->e_flags & EF_ARM_EABI_MASK))
-
-#define compat_start_thread		compat_start_thread
-#define COMPAT_SET_PERSONALITY(ex)		\
-do {						\
-	clear_thread_flag(TIF_32BIT_AARCH64);	\
-	set_thread_flag(TIF_32BIT);		\
-} while (0)
-
-#define COMPAT_ARCH_DLINFO
-extern int aarch32_setup_vectors_page(struct linux_binprm *bprm,
-				      int uses_interp);
-#define compat_arch_setup_additional_pages \
-					aarch32_setup_vectors_page
 
 #endif /* CONFIG_COMPAT */
 
