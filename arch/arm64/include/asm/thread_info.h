0ee77bea2af6dc8dd2a1c3f0123cb41e006b5714
Yury Norov <ynorov@caviumnetworks.com>
2016-05-24 03:04:43 +0300

    arm64: ilp32: add is_ilp32_compat_{task,thread} and TIF_32BIT_AARCH64
    ILP32 tasks are needed to be distinguished from lp64 and aarch32.
    This patch adds helper functions is_ilp32_compat_{task,thread} and
    thread flag TIF_32BIT_AARCH64 to address it. This is a preparation
    for following patches in ilp32 patchset.
    
    For consistency, SET_PERSONALITY is changed here accordingly.
    
    Signed-off-by: Andrew Pinski <Andrew.Pinski@caviumnetworks.com>
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Signed-off-by: Christoph Muellner <christoph.muellner@theobroma-systems.com>
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>
    Reviewed-by: David Daney <ddaney@caviumnetworks.com>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -122,6 +122,7 @@ static inline struct thread_info *current_thread_info(void)
 #define TIF_RESTORE_SIGMASK	20
 #define TIF_SINGLESTEP		21
 #define TIF_32BIT		22	/* AARCH32 process */
+#define TIF_32BIT_AARCH64	23	/* 32 bit process on AArch64(ILP32) */
 
 #define _TIF_SIGPENDING		(1 << TIF_SIGPENDING)
 #define _TIF_NEED_RESCHED	(1 << TIF_NEED_RESCHED)
@@ -133,6 +134,7 @@ static inline struct thread_info *current_thread_info(void)
 #define _TIF_SYSCALL_TRACEPOINT	(1 << TIF_SYSCALL_TRACEPOINT)
 #define _TIF_SECCOMP		(1 << TIF_SECCOMP)
 #define _TIF_32BIT		(1 << TIF_32BIT)
+#define _TIF_32BIT_AARCH64	(1 << TIF_32BIT_AARCH64)
 
 #define _TIF_WORK_MASK		(_TIF_NEED_RESCHED | _TIF_SIGPENDING | \
 				 _TIF_NOTIFY_RESUME | _TIF_FOREIGN_FPSTATE)
