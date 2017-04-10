2f446f2f0b30189197193099992baac9b5c8a3df
Jan Glauber <jglauber@cavium.com>
2017-04-11 01:35:04 +0400

    arm64/locking: qspinlocks and qrwlocks support
    Ported from x86_64 with paravirtualization support removed.
    
    Signed-off-by: Jan Glauber <jglauber@cavium.com>
    
    Note. This patch removes protection from direct inclusion of
    arch/arm64/include/asm/spinlock_types.h. It's done because
    kernel/locking/qrwlock.c file does it thru the header
    include/asm-generic/qrwlock_types.h. Until now the only user
    of qrwlock.c was x86, and there's no such protection too.
    
    I'm not happy to remove the protection, but if it's OK for x86,
    it should be also OK for arm64. If not, I think we'd fix it
    for x86, and add the protection there too.
    
    Yury
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

---
+++ arch/arm64/include/asm/qspinlock.h
@@ -0,0 +1,44 @@
+#ifndef _ASM_ARM64_QSPINLOCK_H
+#define _ASM_ARM64_QSPINLOCK_H
+
+#include <asm-generic/qspinlock_types.h>
+#include <asm/atomic.h>
+
+extern void queued_spin_unlock_wait(struct qspinlock *lock);
+#define queued_spin_unlock_wait queued_spin_unlock_wait
+
+#define	queued_spin_unlock queued_spin_unlock
+/**
+ * queued_spin_unlock - release a queued spinlock
+ * @lock : Pointer to queued spinlock structure
+ */
+static inline void queued_spin_unlock(struct qspinlock *lock)
+{
+	u8 *lock_byte_addr = (u8 *) &lock->val;
+#ifdef CONFIG_CPU_BIG_ENDIAN
+	lock_byte_addr += 3;
+#endif
+	smp_store_release(lock_byte_addr, 0);
+}
+
+#define queued_spin_is_locked queued_spin_is_locked
+/**
+ * queued_spin_is_locked - is the spinlock locked?
+ * @lock: Pointer to queued spinlock structure
+ * Return: 1 if it is locked, 0 otherwise
+ */
+static __always_inline int queued_spin_is_locked(struct qspinlock *lock)
+{
+	/*
+	 * See queued_spin_unlock_wait().
+	 *
+	 * Any !0 state indicates it is locked, even if _Q_LOCKED_VAL
+	 * isn't immediately observable.
+	 */
+	smp_mb();
+	return atomic_read(&lock->val);
+}
+
+#include <asm-generic/qspinlock.h>
+
+#endif /* _ASM_ARM64_QSPINLOCK_H */
