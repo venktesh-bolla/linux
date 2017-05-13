173ee9fd450b10bf7c55497c481289f885e2d579
Yury Norov <ynorov@caviumnetworks.com>
2017-05-13 18:26:51 +0300

    arch: remove GENERIC_FIND_FIRST_BIT
    This option, if disabled, is used to alias find_first_bit() to
    find_next_bit() with the trivial macro:
    #define find_first_bit(addr, size) find_next_bit((addr), (size), 0)
    
    And the same for find_first_zero_bit().
    
    The problem here is that the implementation of find_next_bit() is more
    complex, and that extra complexity is not really needed if the offset
    that passed to find_next_bit() is known to be 0.
    
    This patch removes GENERIC_FIND_FIRST_BIT and drops the alias to find_next_bit().
    Architectures that enable GENERIC_FIND_FIRST_BIT will be obviously not affected
    with this change. Namely, arc, s390, tile, unicore32 and x86. Some architectures
    implement their own implementations, so they are not affected too. They are: arm,
    m68k, unicore32. (Unicore32 both enables the CONFIG_GENERIC_FIND_FIRST_BIT and has
    custom implementation for it, so it fixed too.) Others will switch to separate
    implementation.
    
    Tested on arm64.
    
    There is a couple of patches in the kernel that remove similar config options:
    63e424c84429 ("arch: remove CONFIG_GENERIC_FIND_{NEXT_BIT,BIT_LE,LAST_BIT}") and
    65af3a3f89d7 ("arch: remove CONFIG_GENERIC_FIND_NEXT_BIT again"), but the
    GENERIC_FIND_FIRST_BIT is the last one that still there. So this path finishes
    the work.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- include/asm-generic/bitops/find.h
+++ include/asm-generic/bitops/find.h
@@ -29,8 +29,6 @@ extern unsigned long find_next_zero_bit(const unsigned long *addr, unsigned
 		long size, unsigned long offset);
 #endif
 
-#ifdef CONFIG_GENERIC_FIND_FIRST_BIT
-
 /**
  * find_first_bit - find the first set bit in a memory region
  * @addr: The address to start the search at
@@ -88,11 +86,5 @@ static inline unsigned long find_first_bit(const unsigned long *addr,
  */
 extern unsigned long find_first_zero_bit(const unsigned long *addr,
 					 unsigned long size);
-#else /* CONFIG_GENERIC_FIND_FIRST_BIT */
-
-#define find_first_bit(addr, size) find_next_bit((addr), (size), 0)
-#define find_first_zero_bit(addr, size) find_next_zero_bit((addr), (size), 0)
-
-#endif /* CONFIG_GENERIC_FIND_FIRST_BIT */
 
 #endif /*_ASM_GENERIC_BITOPS_FIND_H_ */
