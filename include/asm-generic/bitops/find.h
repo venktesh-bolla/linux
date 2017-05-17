263c9a4cba3c014c6a77c909aacd01870325ba16
Yury Norov <ynorov@caviumnetworks.com>
2017-05-17 13:12:01 +0300

    find_first{,_zero}_bit: unroll search cycle for small bitmaps
    Small word-aligned bitmaps are common-case in the kernel. This patch optimizes
    the search of first set/clear bits for them.
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- include/asm-generic/bitops/find.h
+++ include/asm-generic/bitops/find.h
@@ -38,8 +38,43 @@ extern unsigned long find_next_zero_bit(const unsigned long *addr, unsigned
  * Returns the bit number of the first set bit.
  * If no bits are set, returns @size.
  */
-extern unsigned long find_first_bit(const unsigned long *addr,
+extern unsigned long __find_first_bit(const unsigned long *addr,
 				    unsigned long size);
+static inline unsigned long find_first_bit(const unsigned long *addr,
+					unsigned long size)
+{
+	unsigned long idx;
+
+	if (!__builtin_constant_p(size))
+		return __find_first_bit(addr, size);
+
+	idx = 0;
+	switch (size) {
+		case BITS_PER_LONG * 4:
+			if (addr[0])
+				return __ffs(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+		case BITS_PER_LONG * 3:
+			if (addr[0])
+				return __ffs(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+		case BITS_PER_LONG * 2:
+			if (addr[0])
+				return __ffs(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+		case BITS_PER_LONG * 1:
+			if (addr[0])
+				return __ffs(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+			return idx;
+	}
+
+	return __find_first_bit(addr, size);
+}
 #endif
 
 #ifndef find_first_zero_bit
@@ -51,8 +86,43 @@ extern unsigned long find_first_bit(const unsigned long *addr,
  * Returns the bit number of the first cleared bit.
  * If no bits are zero, returns @size.
  */
-extern unsigned long find_first_zero_bit(const unsigned long *addr,
+extern unsigned long __find_first_zero_bit(const unsigned long *addr,
 					 unsigned long size);
+static inline unsigned long find_first_zero_bit(const unsigned long *addr,
+						unsigned long size)
+{
+	unsigned long idx;
+
+	if (!__builtin_constant_p(size))
+		return __find_first_zero_bit(addr, size);
+
+	idx = 0;
+	switch (size) {
+		case BITS_PER_LONG * 4:
+			if (~addr[0])
+				return ffz(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+		case BITS_PER_LONG * 3:
+			if (~addr[0])
+				return ffz(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+		case BITS_PER_LONG * 2:
+			if (~addr[0])
+				return ffz(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+		case BITS_PER_LONG * 1:
+			if (~addr[0])
+				return ffz(addr[0]) + idx;
+			addr++;
+			idx += BITS_PER_LONG;
+			return idx;
+	}
+
+	return __find_first_zero_bit(addr, size);
+}
 #endif
 
 #endif /*_ASM_GENERIC_BITOPS_FIND_H_ */
